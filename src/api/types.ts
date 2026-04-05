ba5f7880aea45984ccb03f7138ddc3e69b42d8a0
CiciTang090905 <wtang28@jh.edu>
2026-04-05 14:24:00 -0400

    shopping-cart-02: Data types and API functions

---
+++ src/api/types.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  rating: number;
+  thumbnail: string;
+  images: string[];
+};
+
+
