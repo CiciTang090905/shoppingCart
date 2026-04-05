04548da13a5b2b51c7812eeaa81bfda1af57bd12
CiciTang090905 <wtang28@jh.edu>
2026-04-05 14:30:46 -0400

    shopping-cart-03: Set up TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // ðŸ‘
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree, basepath: "/shopping-cart" });
+const router = createRouter({ routeTree, basepath: "/shoppingCart" });
 
 declare module "@tanstack/react-router" {
   interface Register {
